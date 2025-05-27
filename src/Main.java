512876089404fd5e8c785ba99db086d33c762a55
AlyonaParneva <parneva.aa@gmail.com>
2025-05-27 20:37:55 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер задания (1–4):");
+        int choice = scanner.nextInt();
+        switch (choice) {
+//            case 1 -> task1.run();
+//            case 2 -> task2.run();
+//            case 3 -> task3.run();
+//            case 4 -> task4.run();
+            default -> System.out.println("Такого задания нет :(");
+        }
+    }
+}
